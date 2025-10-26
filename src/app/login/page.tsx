116c19061c28e0df0319cd483812e41ce8f87216
unusali1 <mdunusali1906@gmail.com>
2025-10-26 17:36:23 +0600

    react hook and zod implement form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,42 +13,27 @@ import { Eye, EyeOff, Lock, Mail, UserCheck } from "lucide-react";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-// import { loginUserAction } from "@/features/auth/server/auth.actions";
-import { toast } from "sonner";
 import { loginUserAction } from "@/features/auth/server/auth.actions";
-
-interface LoginFormData {
-  email: string;
-  password: string;
-}
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginUserData, loginUserSchema } from "@/features/auth/auth.schema";
 
 const LoginForm: React.FC = () => {
-  const [formData, setFormData] = useState<LoginFormData>({
-    email: "",
-    password: "",
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginUserSchema),
   });
 
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleInputChange = (name: string, value: string) => {
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
- 
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+  const onSubmit = async (data: LoginUserData) => {
     try {
-      // Here you would typically make your API call
-      const LoginData = {
-        email: formData.email.toLowerCase().trim(),
-        password: formData.password,
-      };
-
-      const result = await loginUserAction(LoginData);
+      const result = await loginUserAction(data);
 
       if (result.status === "SUCCESS") toast.success(result.message);
       else toast.error(result.message);
@@ -67,7 +52,7 @@ const LoginForm: React.FC = () => {
         </CardHeader>
 
         <CardContent>
-          <form onSubmit={handleSubmit}  className="space-y-6">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
             {/* Email Field */}
             <div className="space-y-2">
               <Label htmlFor="email">Email Address *</Label>
@@ -78,13 +63,17 @@ const LoginForm: React.FC = () => {
                   type="email"
                   placeholder="Enter your email"
                   required
-                  value={formData.email}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                    handleInputChange("email", e.target.value)
-                  }
-                  className={`pl-10 `}
+                  {...register("email")}
+                  className={`pl-10 ${
+                    errors.email ? "border-destructive" : ""
+                  }`}
                 />
               </div>
+              {errors.email && (
+                <p className="text-sm text-destructive">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
 
             {/* Password Field */}
@@ -97,11 +86,10 @@ const LoginForm: React.FC = () => {
                   type={showPassword ? "text" : "password"}
                   placeholder="Create a strong password"
                   required
-                  value={formData.password}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                    handleInputChange("password", e.target.value)
-                  }
-                  className={`pl-10 pr-10 `}
+                  {...register("password")}
+                  className={`pl-10 ${
+                    errors.password ? "border-destructive" : ""
+                  }`}
                 />
 
                 <Button
@@ -118,6 +106,11 @@ const LoginForm: React.FC = () => {
                   )}
                 </Button>
               </div>
+              {errors.password && (
+                <p className="text-sm text-destructive">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
 
             {/* Submit Button */}
